9e329d9aea24954b9349249666ae952145dcaefc
Sagia123 <47427081+Sagia123@users.noreply.github.com>
2020-04-24 18:20:33 +0300

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -43,45 +43,87 @@ class graph:
         :param maxIterations: 
         :return: None
         """
-        # s2d = {A: [B, C], B:[D], C: [A, B, D], D: [C]}
+        # A="A"
+        # B="B"
+        # C="C"
+        # D="D"
+        # s2d = {A: [B, C], B: [D], C: [A, B, D], D: [C]}
         # d2s = {A: [C], B: [A, C], C: [A, D], D: [B, C]}
         s2d = self.source_to_dest_dict
         d2s = self.dest_to_source_dict
         
-        
-        max1 = max(s2d.keys(), key=lambda x: int(x))
-        max2 = max(d2s.keys(), key=lambda x: int(x))
-        N = int(max(max1, max2))
-    
+        #Because of dereial ID
+
+        max1 = set(s2d.keys())
+        max2 = set(d2s.keys())
+        unified = max1.union(max2)
+        N = len(unified)
+        print("real number of nodes", N)
+
+
+        if max1 >= max2:
+            allNodes = s2d
+        else:
+            allNodes = d2s
+
+        # N = max(max1, max2)
+        # print("Total nodes number", N)
+
+        #The NULL case
         if N == 0 or N == 1:
-            print("not a valid node realation to run Pagerank")
+            print("not a valid node realations to run Pagerank")
             return
-    
-        pagerank = np.full((N,maxIterations), 0, dtype="float")
-        for i in range(N):
-            pagerank[i][0] = 1/N
-        temp_pagerank = np.full((N, maxIterations), 0, dtype="float")
-        time = range(1, maxIterations)
-        pr_sum = [0]*(len(time)+1)
+
+        time = range(1, maxIterations+1)
+        pagerank = {}
+        temp_pagerank = {}
+
+        for node in s2d.keys():
+            self.page_rank.update({node: 0})
+            pagerank[node] = [0.0]*len(time)
+            temp_pagerank[node] = [0.0]*len(time)
+
+        for jnodes in d2s.keys():
+            pagerank[jnodes] = [0.0]*len(time)
+            pagerank[jnodes][0] = 1/N
+            temp_pagerank[jnodes] = [0.0] * len(time)
+
+
+
+
+
+        pr_sum = [0.0]*(len(time)+1)
         pr_sum[0] = N
-        s=0
+        s = 0.0
         
 
         # The Pagerank algorithm
         for t in time:
-            for temp_node in d2s.keys():
-                for pr_node in d2s[temp_node]:
-                    # r'_j(t)= beta*r_i(t-1)/d_i
-                    temp_pagerank[int(temp_node)-1][t] += beta*pagerank[int(pr_node)-1][t-1]/len(s2d[pr_node])
-                s += temp_pagerank[int(temp_node)-1][t]
-            for temp_node in d2s.keys():
-                pagerank[int(temp_node)-1][t] = temp_pagerank[int(temp_node)-1][t] + (1 - s)/N
-                pr_sum[t] += pagerank[int(temp_node)-1][t]
-            if abs(pr_sum[t]-pr_sum[t-1])<delta:
-                print("We have reached less then delta = ",delta ," and after ",t ," iterations.\n Pagerank scores achieved.")
-                for i in range(N):
-                    self.page_rank.update({str(i): pagerank[i][t]})
+            # to do change below target dict to the dict who hold all nodes
+            # TODO itarate over all nodes
+            for temp_node in unified:    #all j nodes in slide
+                if d2s.get(temp_node):
+                    for pr_node in d2s[temp_node]:  #all i nodes in slide
+                        # r'_j(t)= beta*r_i(t-1)/d_i
+                        temp_pagerank[temp_node][t] += beta*pagerank[pr_node][t-1]/len(s2d[pr_node])
+
+                s += temp_pagerank[temp_node][t]
+            # TODO itarate over all nodes
+            for temp_node in unified:  # all j nodes in slide
+                if d2s.get(temp_node):
+                    pagerank[temp_node][t] = temp_pagerank[temp_node][t] + (1 - s)/N
+                else:
+                    pagerank[temp_node][t] = (1 - s)/N
+                pr_sum[t] += abs(pagerank[temp_node][t] - pagerank[temp_node][t - 1])
+
+            if pr_sum[t] <= delta:
+                print("We have reached less then delta = ", delta, " and after ", t,"iterations.\n Pagerank scores achieved.")
+
+                for temp_node in d2s.keys():
+                    self.page_rank.update({temp_node: pagerank[temp_node][t]})
                 break
+            else:
+                pr_sum[t] = 0
             s = 0
     
         # print("Pagerank algorithm is done")
@@ -134,18 +176,18 @@ class graph:
          """
          
         for i in self.source_to_dest_dict.keys():
-            e1 = len(self.source_to_dest_dict[str(i)])
-            r1 = set(self.source_to_dest_dict[str(i)])
+            e1 = len(self.source_to_dest_dict[i])
+            r1 = set(self.source_to_dest_dict[i])
 
             try:
-                e = e1 + len(self.dest_to_source_dict[str(i)])
-                r = len(r1.union(set(self.dest_to_source_dict[str(i)])))
+                e = e1 + len(self.dest_to_source_dict[i])
+                r = len(r1.union(set(self.dest_to_source_dict[i])))
             except:
                  e = e1
                  r = len(r1)
             finally:
-                if(r == 1 or r == 0): self.cc[str(i)] = 0
-                else: self.cc[str(i)] = float(e/(abs(r)*(abs(r)-1)))
+                if(r == 1 or r == 0): self.cc[i] = 0
+                else: self.cc[i] = float(e/(abs(r)*(abs(r)-1)))
                     
                 
             
@@ -197,3 +239,12 @@ class graph:
         return mean(self.cc[i] for i in self.cc)
         
         
+
+
+# a = graph()
+# a.load_graph("C:/Users/Sagi/Desktop/School/Fourth year/סמסטר ח'/חברתיות/HW/HW1- Pagerank/Wikipedia_votes.csv")
+# a.calculate_page_rank()
+# for s in a.get_top_PageRank(10):
+#     print(s)
+# a.calcuate_CC()
+# print(a.get_top_CC(5))
